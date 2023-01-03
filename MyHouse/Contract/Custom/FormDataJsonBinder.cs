73f064d6bc34fc4e8c87f44695b51de9b33c0256
Trung Nguyễn <trung.nguyen@veek.vn>
2023-01-03 09:37:44 +0700

    init

---
+++ MyHouse/Contract/Custom/FormDataJsonBinder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Contract.Custom
+{
+    public class FormDataJsonBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            var modelName = bindingContext.ModelName;
+
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(modelName);
+
+            if (valueProviderResult == ValueProviderResult.None)
+            {
+                return Task.CompletedTask;
+            }
+
+            bindingContext.ModelState.SetModelValue(modelName, valueProviderResult);
+
+            var value = valueProviderResult.FirstValue;
+
+            if (string.IsNullOrEmpty(value))
+            {
+                return Task.CompletedTask;
+            }
+
+            try
+            {
+                var result = JsonSerializer.Deserialize(value, bindingContext.ModelType);
+                bindingContext.Result = ModelBindingResult.Success(result);
+            }
+            catch (Exception ex)
+            {
+                bindingContext.Result = ModelBindingResult.Failed();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
